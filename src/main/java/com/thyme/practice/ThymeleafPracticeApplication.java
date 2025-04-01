df123c2ed5e74dd2fda21ca1b74a7f4fe7e631bf
zaheerkhan81 <zaheerkhan81@gmail.com>
2025-04-01 12:26:17 +0530

    commit the code from eclipse

--- src/main/java/com/thyme/practice/ThymeleafPracticeApplication.java
+++ src/main/java/com/thyme/practice/ThymeleafPracticeApplication.java
@@ -2,13 +2,24 @@ package com.thyme.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ThymeleafPracticeApplication {
+public class ThymeleafPracticeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ThymeleafPracticeApplication.class, args);
 		System.out.println(" INSIDE MAIN METHOD");
-	}
 
+		SpringApplication.run(ThymeleafPracticeApplication.class, args);
+		
+		
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		System.out.println(" INSIDE MAIN METHOD BUILD");
+
+		return builder.sources(ThymeleafPracticeApplication.class);
+	}
+	
 }
--- src/main/java/com/thyme/practice/controller/HelloWordController.java
+++ src/main/java/com/thyme/practice/controller/HelloWordController.java
@@ -1,17 +1,26 @@
 package com.thyme.practice.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 
-@RestController
+@Controller
 public class HelloWordController {
 
 	
 	@GetMapping("/Hello")
     private String getEmployeeDetails() {
+		return "Hello Word";
+    }
+	
+	@RequestMapping("/home")
+    public String gethome(Model model) {
        
-        return "Hello Word";
+    	model.addAttribute("message", "Welcome to Spring Boot with JSP!");
+	        return "home"; // home.jsp
     }
 }
